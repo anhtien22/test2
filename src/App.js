65c25cae40e07ea532466bc16d9d2178c0bd128b
anhtien22 <anhtien27722@gmail.com>
2024-08-14 23:16:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [phone, setPhone] = useState('');
+
+  const handleInputChange = (e) => {
+    let input = e.target.value.replace(/\D/g, '');
+    if (input.length > 3 && input.length <= 6) {
+      input = `(${input.slice(0, 3)})${input.slice(3)}`;
+    } else if (input.length > 6) {
+      input = `(${input.slice(0, 3)})${input.slice(3, 6)}-${input.slice(6, 10)}`;
+    }
+
+    setPhone(input);
+  };
+
+  const handleKeyDown = (e) => {
+    const allowedKeys = ['Backspace', 'Delete', 'ArrowLeft', 'ArrowRight'];
+    if (!allowedKeys.includes(e.key) && !/^\d$/.test(e.key)) {
+      e.preventDefault();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <input
+      type="text"
+      value={phone}
+      onChange={handleInputChange}
+      onKeyDown={handleKeyDown}
+      maxLength="14"
+      placeholder="(123)456-7890"
+    />
   );
-}
+};
+
 
 export default App;
